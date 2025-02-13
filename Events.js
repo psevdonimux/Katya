228c139ff54e9d1bf50cf017a96c7839f06fa5c3
Rina-5510 <Vetrova5510@gmail.com>
2025-02-13 21:13:23 +0500

    Update Events.js

--- Events.js
+++ Events.js
@@ -1,4 +1,4 @@
-function eventsByDay = {
+const eventsByDay = {
     1: [ // Понедельник
         { time: new Date().setHours(9, 0), label: 'Разговоры', type: 'type1', duration: 30 * 60 * 1000},
         { time: new Date().setHours(9, 45), label: 'Физ-ра', type: 'type1', duration: 40 * 60 * 1000},
