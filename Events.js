9fa9397797fa5c8645cc407d22967bfb206eb284
Rina-5510 <Vetrova5510@gmail.com>
2025-02-14 19:13:19 +0500

    Update Events.js

--- Events.js
+++ Events.js
@@ -149,28 +149,4 @@ class events {
                 
             ]
         };
-    millisecondsInDay = 24 * 60 * 60 * 1000;
-    startOfDay = new Date().setHours(0, 0, 0, 0);
-    createEvents () {
-        function displayEvents(day = -1) {
-            const timeline = document.querySelector('.timeline');
-            const timelineWidth = timeline.offsetWidth;
-            const existingEvents = timeline.querySelectorAll('.event');
-            existingEvents.forEach(event => event.remove());
-            const currentDay = day == -1 ? new Date().getDay() : day;
-            eventsByDay[currentDay].forEach(event => {
-                const eventElement = document.createElement('div');
-                eventElement.classList.add('event');
-                eventElement.classList.add(event.type);
-                eventElement.style.left = ((((new Date(event.time)) - startOfDay) / millisecondsInDay) * timelineWidth) + 'px';
-                eventElement.style.width = ((event.duration / millisecondsInDay) * timelineWidth) + 'px';
-                eventElement.textContent = event.label;
-                timeline.appendChild(eventElement);
-            });
-        }
-        function handleResize() {
-            displayEvents();
-        }
-        displayEvents();
-    }
 }
