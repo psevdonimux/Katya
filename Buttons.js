c81426bd76befa1176fe2704fc4205b418370af0
Rina-5510 <Vetrova5510@gmail.com>
2025-02-16 01:47:23 +0500

    Update Buttons.js

--- Buttons.js
+++ Buttons.js
@@ -52,6 +52,17 @@ class Buttons {
           timeline.appendChild(eventElement);
         });
       }
+  scrollToRedLine() {
+    const movingLine = document.getElementById('movingLine');
+    const chatgpt = document.getElementById('scroll');
+    scroll.onclick = function() {
+      movingLine.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+        inline: 'center'
+      });
+    }
+  } 
 }
 const but = new Buttons();
 but.displayEvents();
