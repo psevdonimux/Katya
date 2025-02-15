e05dd42c47fccd9c51a99578a50cd9809c9df825
Rina-5510 <Vetrova5510@gmail.com>
2025-02-15 16:03:29 +0500

    Update Buttons.js

--- Buttons.js
+++ Buttons.js
@@ -32,7 +32,32 @@ class Buttons {
         }
       };
     });
+    displayEvents(data-day);
   }
+  displayEvents(day = -1) {
+            const millisecondsInDay = 24 * 60 * 60 * 1000;
+            const startOfDay = new Date().setHours(0, 0, 0, 0);
+        const timeline = document.querySelector('.timeline');
+        const timelineWidth = timeline.offsetWidth;
+        const existingEvents = timeline.querySelectorAll('.event');
+        existingEvents.forEach(event => event.remove());
+        const currentDay = day == -1 ? new Date().getDay() : day;
+        eventsByDay[currentDay].forEach(event => {
+          const eventElement = document.createElement('div');
+          eventElement.classList.add('event');
+          eventElement.classList.add(event.type);
+          eventElement.style.left = ((((new Date(event.time)) - startOfDay) / millisecondsInDay) * timelineWidth) + 'px';                               
+          eventElement.style.width = ((event.duration / millisecondsInDay) * timelineWidth) + 'px';
+          eventElement.textContent = event.label;
+          timeline.appendChild(eventElement);
+        });
+      }
+function handleResize() {
+        displayEvents();
+      }
+window.addEventListener('resize', handleResize);
+      displayEvents();
+
 }
 const but = new Buttons();
 window.addEventListener('load', function () {
